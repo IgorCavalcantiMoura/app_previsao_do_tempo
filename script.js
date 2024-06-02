9e8ba96bd28725694c957f0b7d639a71a917010a
IgorCavalcantiMoura <icavalcantimoura@gmail.com>
2024-06-02 01:26:57 -0300

    feat: interface e consumo de api

---
+++ script.js
@@ -0,0 +1,29 @@
+const key = "43a10697d042e639c0ad528c3cc7999d";
+
+
+function mostrarDados(dados){
+    document.querySelector(".cidade").innerHTML= "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".img-previsao").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+}
+
+async function buscarCidade(cidade) {
+  const dados = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cidade +
+      "&appid=" +
+      key +
+      "&lang=pt_br" +
+      "&units=metric"
+  ).then((resposta) => resposta.json());
+
+  mostrarDados(dados);
+}
+
+function cliqueBotao() {
+  const cidade = document.querySelector(".input-cidade").value;
+
+  buscarCidade(cidade);
+}
